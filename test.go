8d0523117e5b177e20f709d48ac49514552f4156
Manatsanan10 <45794724+Manatsanan55@users.noreply.github.com>
2020-02-23 22:06:24 +0700

    Test

--- test.go
+++ test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
 	"os"
 )
 
 func main() {
 	_, err := os.Stat("C:/New Folder")
 	if os.IsNotExist(err) {
+		errDir := os.MkdirAll("C:/New folder", 0755)
+		log.Fatal(err)
 	}
 
 }
